2238ded16815999df580339d3437a3b30c94adf8
olivergoodman <ollygreatman@gmail.com>
2015-10-12 14:02:27 -0500

    commit

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 	console.log("it's working");
-	//$('div.hidden').fadeIn(2000).removeClass("hidden"); title not fading right now
-	$('div.slideR').animate({right: "-500px"}, 1000);
+	$('div.hidden').fadeIn(2000).removeClass("hidden"); //title not fading right now
+	//$('div.slideR').animate({right: "-500px"}, 1000);
 	$('div.slideL').animate({left: "-500px"}, 1000);
 });
