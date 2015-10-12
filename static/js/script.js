79bc0b06a538f2f32d7642f5d614a7c46942b1e5
olivergoodman <ollygreatman@gmail.com>
2015-10-12 13:52:18 -0500

    first commit - jquery
    starting w/ jQuery, will try CSS

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 	console.log("it's working");
-	//$('div.hidden').fadeIn(2000).removeClass("hidden"); title not fading right now
-	$('div.slideR').animate({right: "-500px"}, 1000);
-	$('div.slideL').animate({left: "-500px"}, 1000);
+	$('div.hidden').fadeIn(2000).removeClass("hidden");
+	//$('div.slideR').animate({left: "1000px"}, 1000); //creating excess space on right side.
+	$('div.slideL').animate({right: "500px"}, 1000);
 });
